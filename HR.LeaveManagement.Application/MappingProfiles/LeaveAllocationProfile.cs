369e4046704a2a8d7a74255c2f15d70fd36fef45
Dominic James <dominic@bepeerless.co>
2024-07-21 00:40:33 +0100

    edited mapper

--- HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.UpdateLeaveAllocation;
 using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetAllLeaveAllocations;
 using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
 using HR.LeaveManagement.Domain;
@@ -13,7 +14,7 @@ namespace HR.LeaveManagement.Application.MappingProfiles
             CreateMap<LeaveAllocationDto, LeaveAllocation>().ReverseMap();
             CreateMap<LeaveAllocation, LeaveAllocationsDetailsDto>();
             CreateMap<CreateLeaveAllocationCommand, LeaveAllocation>();
-            //CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
+            CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
         }
     }
 }
