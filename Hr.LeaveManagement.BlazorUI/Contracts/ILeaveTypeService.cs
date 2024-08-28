215188766f7cb90069d75541cbafc80310cd2b93
Dominic James <dominic@bepeerless.co>
2024-08-29 00:08:47 +0100

    configuring the services for the UI and addind a generic response

--- Hr.LeaveManagement.BlazorUI/Contracts/ILeaveTypeService.cs
+++ Hr.LeaveManagement.BlazorUI/Contracts/ILeaveTypeService.cs
@@ -1,7 +1,15 @@
-﻿namespace Hr.LeaveManagement.BlazorUI.Contracts
+﻿using Hr.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using Hr.LeaveManagement.BlazorUI.Services.Base;
+
+namespace Hr.LeaveManagement.BlazorUI.Contracts
 {
     public interface ILeaveTypeService
     {
+        Task<List<LeaveTypeVM>> GetLeaveTypes();
+        Task<LeaveTypeVM> GetLeaveTypeDetails(int id);
+        Task<Response<Guid>> CreateLeaveType(LeaveTypeVM leaveType);
+        Task<Response<Guid>> UpdateLeaveType(int id, LeaveTypeVM leaveType);
+        Task<Response<Guid>> DeleateLeaveType(int id);
 
     }
 }
