3e45ac795f0481f5186c7d4a9874a8b11954f723
Dominic James <dominic@bepeerless.co>
2024-09-30 12:08:08 +0100

    fixing frontend

---
+++ HR.LeaveManagement.Identity/Services/AuthService.cs
@@ -0,0 +1,77 @@
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Identity.Models;
+using HR.LeaveManagement.Identity.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IOptions<JwtSettings> _jwtSettings;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthService(UserManager<ApplicationUser> userManager,
+            IOptions<JwtSettings> jwtSettings,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _jwtSettings = jwtSettings;
+            _signInManager = signInManager;
+        }
+
+
+        public async Task<AuthResponse> Login(AuthRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                throw new NotFoundException($"User with {request.Email} not Found", request.Email);
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+            if (!result.Succeeded)
+            {
+                throw new BadRequestException($"Credentials for {request.Email}arent valid");
+            }
+
+            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+        }
+
+        private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = roles.Select(q => new Claim(ClaimTypes.Role, q)).ToList();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+
+                new Claim ("uid", user.Id)
+            }xxxxxx
+            .Union(userClaims)
+            .Union(roleClaims);
+        }
+
+        public Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
